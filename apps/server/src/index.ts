fa48e9a6b9bf99b9a55e05fadd0493e5d8f7e6a6
Ergiaa <titovirgiawan21@gmail.com>
2026-03-24 16:53:33 +0700

    format: run biome format on repo

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,13 +3,13 @@ import { env } from "@scholar-seek/env/server";
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-  .use(
-    cors({
-      origin: env.CORS_ORIGIN,
-      methods: ["GET", "POST", "OPTIONS"],
-    }),
-  )
-  .get("/", () => "OK")
-  .listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-  });
+	.use(
+		cors({
+			origin: env.CORS_ORIGIN,
+			methods: ["GET", "POST", "OPTIONS"],
+		})
+	)
+	.get("/", () => "OK")
+	.listen(3000, () => {
+		console.log("Server is running on http://localhost:3000");
+	});
