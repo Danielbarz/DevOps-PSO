5c54129012a05374cc6a3f5852a8a75826f31e03
Daniel Bara Seftino <142491841+Danielbarz@users.noreply.github.com>
2026-06-10 08:58:15 +0700

    Update treaty.ts

--- apps/web/src/lib/api/treaty.ts
+++ apps/web/src/lib/api/treaty.ts
@@ -1,7 +1,11 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@scholar-seek/server";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL ?? "http://localhost:3000";
+// Otomatis pakai URL Azure di Production, dan localhost di Development
+const SERVER_URL =
+  import.meta.env.PROD && typeof window !== "undefined"
+    ? window.location.origin
+    : import.meta.env.VITE_SERVER_URL ?? "http://localhost:3000";
 
 export const api = treaty<App>(SERVER_URL);
 
